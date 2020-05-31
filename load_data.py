92090c7554ca74ca6a0943f63cbf58a5df4c36a1
sean wayland <seanwayland@gmail.com>
2020-05-31 17:31:04 -0400

    load_data new data

--- load_data.py
+++ load_data.py
@@ -4,24 +4,25 @@ import pymongo
 import pandas as pd
 import numpy as np
 import datetime as datetime
+
 #import matplotlib.pyplot as pltconda
 #import plotly.graph_objects as go
 
 # function which collects data from CSV files and dumps into mongo DB database
 def load():
 
-    GOLD_df = pd.read_csv('data/GOLD_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
-    DJIA_df = pd.read_csv('data/DJIA_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
+    #GOLD_df = pd.read_csv('data/GOLD_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
+    #DJIA_df = pd.read_csv('data/DJIA_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
+
+    combo_df = pd.read_csv('data/dow_gold_div_change_1915_2020.csv', encoding='utf-8', delimiter=',', engine='python')
+   # DJIA_df.rename(columns={"yearmonth_str": "date"})
 
 
-    DJIA_df.rename(columns={"yearmonth_str": "date"})
+    #COMBINED_df = pd.merge(GOLD_df, DJIA_df)
 
+    #COMBINED_df = COMBINED_df.rename(columns={'nominal_price_USD': 'gold_nominal_price_USD'})
 
-    COMBINED_df = pd.merge(GOLD_df, DJIA_df)
-
-    COMBINED_df = COMBINED_df.rename(columns={'nominal_price_USD': 'gold_nominal_price_USD'})
-
-    print(COMBINED_df)
+    #print(COMBINED_df)
     conn = 'mongodb://localhost:27017'
 
         # Pass connection to the pymongo instance.
@@ -35,7 +36,7 @@ def load():
     # recreate it
     db = client.myDB.dowGoldHistory
     # convert pandas DF to dict
-    data = COMBINED_df.to_dict(orient='records')  # Here's our added param..
+    data = combo_df.to_dict(orient='records')  # Here's our added param..
     # dump it it
     db.insert_many(data)
     # query the DB
