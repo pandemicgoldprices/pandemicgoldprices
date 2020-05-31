36287c30571d5d2a6b3a9466e55297f9d15b11d3
seanwayland <seanwayland@gmail.com>
2020-05-31 17:47:09 -0400

    loads newest data

--- load_data.py
+++ load_data.py
@@ -15,6 +15,7 @@ def load():
     #DJIA_df = pd.read_csv('data/DJIA_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
 
     combo_df = pd.read_csv('data/dow_gold_div_change_1915_2020.csv', encoding='utf-8', delimiter=',', engine='python')
+    deaths_df = pd.read_csv('data/pandemicmonthlydeaths.csv', encoding='utf-8', delimiter=',', engine='python')
    # DJIA_df.rename(columns={"yearmonth_str": "date"})
 
 
@@ -31,18 +32,32 @@ def load():
         # Connect to a database. Will create one if not already available.
 
     db = client.myDB.dowGoldHistory
+    db2 = client.myDB.pandemicDeaths
     # clear the DB
     db.drop()
+    db2.drop()
     # recreate it
     db = client.myDB.dowGoldHistory
+    db2 = client.myDB.pandemicDeaths
+
     # convert pandas DF to dict
-    data = combo_df.to_dict(orient='records')  # Here's our added param..
+    data = combo_df.to_dict(orient='records')
+     # Here's our added param..
     # dump it it
+    data2 = deaths_df.to_dict(orient='records')
     db.insert_many(data)
+    db2.insert_many(data2)
     # query the DB
     dataOut = db.find().sort('_id', -1)
     # count the records
     results_count = str(dataOut.count())
-    print("results count :" + results_count)
+    print("results count of gold dow data :" + results_count)
     # display top record
     print(dataOut[0])
+
+    dataOut2 = db2.find().sort('_id', -1)
+        # count the records
+    results_count2 = str(dataOut2.count())
+    print("results count of deaths data :" + results_count2)
+        # display top record
+    print(dataOut2[0])
