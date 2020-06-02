0822de17cc6806fdacf8fe9bf2e8b5bd8b128e13
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-06-01 21:30:14 -0400

    Aussie assistance

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template, jsonify, redirect, url_for, request
 # from scrape import scrape
-from dao.database import getLatest,  getRangeDowData, getMonthlyDeathsData
+from dao.database import getLatest, getRangeDowData, getMonthlyDeathsData
 from load_data import load
 
 import json
@@ -12,7 +12,6 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
-
 
 
 load()
@@ -54,8 +53,10 @@ def template_data():
 
 @app.route('/spanish_flu_1918')
 def spanish_flu():
-    spanish_flu_data = getRangeDowData(1918,1919)
+    spanish_flu_data = getRangeDowData(1917,1922)
     json_spanish_flu_data = str(JSONEncoder().encode(spanish_flu_data))
+    print("Spanish Flu")
+    print(json_spanish_flu_data)
     return render_template('spanish_flu.html', data = json_spanish_flu_data)
 
 @app.route('/asian_flu')
@@ -80,25 +81,18 @@ def coronavirus():
     return render_template('coronavirus.html', data = data)
 
 
-
-
-''' test route that renders data from mongo into a plotly graph 
+''' test route that renders data from mongo into a plotly graph...
 you can select the years for your pandemic in getRangeDowData
 '''
 
 @app.route('/render_test')
 def renderTest():
     newData = getRangeDowData(1918,1920)
-
     jsonNewData2 = str(JSONEncoder().encode(newData))
     #print(jsonNewData2)
-
     deathData = getMonthlyDeathsData()
     jsonNewData3 = str(JSONEncoder().encode(deathData))
-
     return render_template('render_test.html', data = jsonNewData2, deathData = jsonNewData3)
-    
-
 
 # run the app
 if __name__ == '__main__':
