1152cc45dda325504e2a99a1438b7f3f5a6b26b2
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-06-07 16:10:30 -0400

    simple, diligent changes for readability, no functionalities added

--- load_data.py
+++ load_data.py
@@ -5,32 +5,29 @@ import pandas as pd
 import numpy as np
 import datetime as datetime
 
-#import matplotlib.pyplot as pltconda
-#import plotly.graph_objects as go
+# import matplotlib.pyplot as pltconda
+# import plotly.graph_objects as go
 
 # function which collects data from CSV files and dumps into mongo DB database
 def load():
 
-    #GOLD_df = pd.read_csv('data/GOLD_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
-    #DJIA_df = pd.read_csv('data/DJIA_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
-
+    # GOLD_df = pd.read_csv('data/GOLD_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
+    # DJIA_df = pd.read_csv('data/DJIA_monthly_cleaned.csv', encoding='utf-8', delimiter=',', engine='python')
     combo_df = pd.read_csv('data/dow_real_gold_div_change_1915_2020_cpi_with_charts.csv', encoding='utf-8', delimiter=',', engine='python')
     deaths_df = pd.read_csv('data/pandemicmonthlydeathsrealgold.csv', encoding='utf-8', delimiter=',', engine='python')
-   # DJIA_df.rename(columns={"yearmonth_str": "date"})
+    # DJIA_df.rename(columns={"yearmonth_str": "date"})
 
+    # COMBINED_df = pd.merge(GOLD_df, DJIA_df)
 
-    #COMBINED_df = pd.merge(GOLD_df, DJIA_df)
+    # COMBINED_df = COMBINED_df.rename(columns={'nominal_price_USD': 'gold_nominal_price_USD'})
 
-    #COMBINED_df = COMBINED_df.rename(columns={'nominal_price_USD': 'gold_nominal_price_USD'})
-
-    #print(COMBINED_df)
+    # print(COMBINED_df)
     conn = 'mongodb://localhost:27017'
 
-        # Pass connection to the pymongo instance.
+    # Pass connection to the pymongo instance.
     client = pymongo.MongoClient(conn)
 
-        # Connect to a database. Will create one if not already available.
-
+    # Connect to a database. Will create one if not already available.
     db = client.myDB.dowGoldHistory
     db2 = client.myDB.monthlydeaths
     # clear the DB
@@ -55,10 +52,10 @@ def load():
 
     db2.insert_many(data2)
 
-        # query the DB
+    # query the DB
     dataOut2 = db2.find().sort('_id', -1)
-        # count the records
+    # count the records
     results_count2 = str(dataOut2.count())
     print("results count deaths :" + results_count2)
-        # display top record
+    # display top record
     print(dataOut2[0])
